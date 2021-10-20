1e7fa99288975cdc695d3e8e4a67c7c6db46be2f
Anne-G <ang.schall@gmail.com>
2021-10-20 17:13:56 +0200

    wip

--- create-branch-and-push.sh
+++ create-branch-and-push.sh
@@ -1,5 +1,7 @@
+git checkout dev
 git checkout -b test-$1
 echo "hello world $1" > hello-world-$1.md
 git add .
 git commit -m "Add hello world $1"
 git push --set-upstream origin test-$1
+git checkout dev
